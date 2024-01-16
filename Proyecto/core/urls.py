da8d3c66b01bae6073474ef48998e9199ca4c99a
Martin Avelen <martinboyb@gmail.com>
2024-01-16 19:36:09 -0300

    Add templates

--- Proyecto/core/urls.py
+++ Proyecto/core/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from . import views
 
+app_name = "core"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("profesor/list", views.profesor_list, name="profesor_list"),
-    path("estudiante/list", views.estudiante_list, name="estudiante_list"),
-    path("profesor/create", views.profesor_create, name="profesor_create"),
-    path("estudiante/create", views.estudiante_create, name="estudiante_create"),
-    path("curso/list", views.curso_list, name="curso_list"),
-    path("curso/create", views.curso_create, name="curso_create"),
+    path("estudiante/list/", views.estudiante_list, name="estudiante_list"),
+    path("profesor/create/", views.profesor_create, name="profesor_create"),
+    path("estudiante/create/", views.estudiante_create, name="estudiante_create"),
+    path("curso/list/", views.curso_list, name="curso_list"),
+    path("curso/create/", views.curso_create, name="curso_create"),
     
     
 ]
